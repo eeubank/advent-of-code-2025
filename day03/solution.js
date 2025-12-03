bee54e305f726c6ecf14c0d57e69d90f6864eccb
Eric Eubank <eric@characterstrong.com>
2025-12-03 08:42:08 -0500

    Day 3

--- day03/solution.js
+++ day03/solution.js
@@ -1,5 +1,37 @@
 async function solution(input) {
-  console.log(input);
+  let joltage = 0;
+  let unsafeJoltage = 0;
+
+  for ( line of input ) {
+    joltage += getMaxJoltage(line, 2);
+    unsafeJoltage += getMaxJoltage(line, 12);
+  }
+
+  console.log(`Joltage: ${joltage}`);
+  console.log(`Unsafe Joltage: ${unsafeJoltage}`);
+}
+
+function getMaxJoltage(battery, batteryLimit) {
+  const arr = battery.split('');
+
+  const batteries = new Array(batteryLimit).fill(0);
+
+  let currBattery = 0;
+  let firstIdx = 0;
+  
+  while(currBattery < batteryLimit) {
+    for ( let i = firstIdx; i < arr.length - (batteryLimit - (currBattery + 1)); i++ ) {
+      const num = +arr[i];
+      if (num > batteries[currBattery]) {
+        batteries[currBattery] = num;
+        firstIdx = i + 1;
+      }
+    }
+
+    currBattery++;
+  }
+
+  return +batteries.join('');
 }
 
 module.exports = solution;
--- main.js
+++ main.js
@@ -5,7 +5,11 @@ async function main() {
   const { day, dataset } = readArgs();
   const input = await readInput(`./${day}/input_${dataset}.txt`);
 
+  const startTime = Date.now();
   await require(`./${day}/solution`)(input);
+  const endtime = Date.now();
+
+  console.log(`Time: ${endtime - startTime} ms`);
 }
 
 main();
